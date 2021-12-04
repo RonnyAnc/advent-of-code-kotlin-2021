c83fca6c1a79032c6cce27e93f8ed41621eb2609
Ronny Ancorini Rodríguez <ronnyancorini@outlook.com>
2021-12-04 19:32:50 +0100

    [GREEN] not_detect_an_increment_when_a_measure_is_not_bigger_than_the_previous_one

--- src/test/kotlin/DetectIncrementsShould.kt
+++ src/test/kotlin/DetectIncrementsShould.kt
@@ -8,13 +8,19 @@ class DetectIncrementsShould {
     }
 
     @Test
-    fun detect_an_increment_when_next_input_is_bigger_than_the_previous_one() {
+    fun detect_an_increment_when_a_measure_is_bigger_than_the_previous_one() {
         assertEquals(1, detectIncrement(listOf(0, 1)))
+    }
+
+    @Test
+    fun not_detect_an_increment_when_a_measure_is_not_bigger_than_the_previous_one() {
+        assertEquals(0, detectIncrement(listOf(1, 1)))
     }
 
     private fun detectIncrement(emptyList: List<Int>): Int {
         if (emptyList.size > 1)
-            return 1
+            if (emptyList.first() < emptyList.last())
+                return 1
         return 0
     }
 }
