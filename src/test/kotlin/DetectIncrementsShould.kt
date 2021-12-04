e7d72362d4e77e86e0e73bf4c1fa5972d13c6a9d
Ronny Ancorini Rodríguez <ronnyancorini@outlook.com>
2021-12-04 19:28:31 +0100

    [GREEN] detect_an_increment_when_next_input_is_bigger_than_the_previous_one

--- src/test/kotlin/DetectIncrementsShould.kt
+++ src/test/kotlin/DetectIncrementsShould.kt
@@ -7,7 +7,14 @@ class DetectIncrementsShould {
         assertEquals(0, detectIncrement(emptyList()))
     }
 
+    @Test
+    fun detect_an_increment_when_next_input_is_bigger_than_the_previous_one() {
+        assertEquals(1, detectIncrement(listOf(0, 1)))
+    }
+
     private fun detectIncrement(emptyList: List<Int>): Int {
+        if (emptyList.size > 1)
+            return 1
         return 0
     }
 }
