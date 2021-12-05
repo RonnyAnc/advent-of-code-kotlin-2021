628eafe9cb7d60d453f0258edad0919f78ad7748
Ronny Ancorini Rodríguez <ronnyancorini@outlook.com>
2021-12-05 22:10:31 +0100

    [GREEN] retrieve non 0 when it was moved horizontally and vertically

--- src/test/kotlin/day2Dive/RequestSubmarineMoveShould.kt
+++ src/test/kotlin/day2Dive/RequestSubmarineMoveShould.kt
@@ -3,6 +3,7 @@ package day2Dive
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
+import kotlin.io.path.writeText
 
 class RequestSubmarineMoveShould {
     @Test
@@ -12,7 +13,23 @@ class RequestSubmarineMoveShould {
         assertEquals(0, result)
     }
 
-    private fun requestSubmarineBatchMovements(movementRequestsFile: Path): Int {
+    @Test
+    fun `retrieve non 0 when it was moved horizontally and vertically`() {
+        val movementRequestsFile = kotlin.io.path.createTempFile()
+        movementRequestsFile.writeText("""
+            forward 1
+            down 1
+        """.trimIndent())
+        val result = requestSubmarineBatchMovements(movementRequestsFile)
+        assertEquals(1, result)
+    }
+
+    private fun requestSubmarineBatchMovements(movementsFilePath: Path): Int {
+        var movement = 0
+        movementsFilePath.toFile().forEachLine { when {
+            it.startsWith("forward") -> movement += 1
+        } }
+        if (movement != 0 ) return 1
         return 0
     }
 }
