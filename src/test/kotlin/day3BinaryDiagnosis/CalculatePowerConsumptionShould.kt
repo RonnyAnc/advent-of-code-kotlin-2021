1839e99bd3b34e41b6e10c25dfe840fa0a7dba4e
Ronny Ancorini Rodríguez <ronnyancorini@outlook.com>
2021-12-06 18:55:22 +0100

    [GREEN] return when file contains a single 1 bit

--- src/test/kotlin/day3BinaryDiagnosis/CalculatePowerConsumptionShould.kt
+++ src/test/kotlin/day3BinaryDiagnosis/CalculatePowerConsumptionShould.kt
@@ -3,6 +3,7 @@ package day3BinaryDiagnosis
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
+import kotlin.io.path.writeText
 
 class CalculatePowerConsumptionShould {
     @Test
@@ -12,7 +13,22 @@ class CalculatePowerConsumptionShould {
         assertEquals(0, powerConsumption)
     }
 
+    @Test
+    fun `return when file contains a single 1 bit`() {
+        val diagnosisReportPath = kotlin.io.path.createTempFile()
+        diagnosisReportPath.writeText("""
+            1
+        """.trimIndent())
+        val powerConsumption = calculatePowerConsumption(diagnosisReportPath)
+        assertEquals(1, powerConsumption)
+    }
+
     private fun calculatePowerConsumption(diagnosisReportFile: Path): Int {
+        var containsAnyLine = false
+        diagnosisReportFile.toFile().forEachLine {
+            containsAnyLine = true
+        }
+        if (containsAnyLine) return 1
         return 0
     }
 }
